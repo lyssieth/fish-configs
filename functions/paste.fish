df2befb01e41692bb743cccda0e9c2cf85706e58
Lyssieth <lys@lys.ee>
2023-09-18 01:15:37 +0300

    fix: make paste able to pipe

--- functions/paste.fish
+++ functions/paste.fish
@@ -1,3 +1,8 @@
 function paste --wraps='wl-paste'
-    wl-paste >$argv[1]
+    # test if we have argv[1]
+    if test -z "$argv[1]"
+        wl-paste
+    else
+        wl-paste >$argv[1]
+    end
 end
