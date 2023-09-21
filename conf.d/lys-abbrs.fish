15cd4ffecada5335514f8b57768d93c74afbcff0
Lys <lys@lys.ee>
2023-09-22 00:21:21 +0300

    chore: improve code application
    a

--- conf.d/lys-abbrs.fish
+++ conf.d/lys-abbrs.fish
@@ -1,10 +1,12 @@
 # check if code-insiders exists
-if ! command -v code-insiders &>/dev/null
-    echo "code-insiders could not be found, using code instead"
-    abbr --add c. code .
-else
-    abbr --add c. code-insiders .
-    abbr --add code code-insiders
+if status --is-interactive
+    if ! command -v code-insiders &>/dev/null
+        echo "code-insiders could not be found, using code instead"
+        abbr --add c. code .
+    else
+        abbr --add c. code-insiders .
+        abbr --add code code-insiders
+    end
 end
 
 abbr --add gca git commit -a
